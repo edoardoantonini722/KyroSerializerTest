e0d093bf93a79c95ef12f7f4eac4eefc0442cd43
Edoardo Antonini <edoardo.antonini@studio.unibo.it>
2016-08-02 19:44:44 +0200

    first try

--- kyroSerializerTest/src/main/java/com/magnews/kyroSerializerTest/App.java
+++ kyroSerializerTest/src/main/java/com/magnews/kyroSerializerTest/App.java
@@ -1,13 +1,22 @@
 package com.magnews.kyroSerializerTest;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import java.util.Properties;
+import javax.cache.Cache;
+import javax.cache.CacheManager;
+import javax.cache.Caching;
+import javax.cache.configuration.MutableConfiguration;
+import javax.cache.spi.CachingProvider;
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+	public static void main(String ... args) {
+        CachingProvider provider = Caching.getCachingProvider();
+        Properties properties = new Properties();
+        try (CacheManager cacheManager = provider.getCacheManager(provider.getDefaultURI(), provider.getDefaultClassLoader(), properties)) {
+            MutableConfiguration<String, String> cacheConfiguration = new MutableConfiguration<>();
+            Cache<String, String> cache = cacheManager.createCache("example", cacheConfiguration);
+            cache.put("foo", "bar");
+            String result = cache.get("foo");
+            System.out.println("result:" + result);
+        }
     }
 }
